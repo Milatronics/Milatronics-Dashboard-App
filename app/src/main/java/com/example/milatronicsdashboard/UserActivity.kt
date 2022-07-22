600c729490afacd88547766308abae46e5ad4d0c
Taha_Adeel <cs20btech11052@iith.ac.in>
2022-07-23 01:03:45 +0530

    Added navigation and fragments for the bottom navigation bar

--- app/src/main/java/com/example/milatronicsdashboard/UserActivity.kt
+++ app/src/main/java/com/example/milatronicsdashboard/UserActivity.kt
@@ -4,9 +4,12 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.amplifyframework.auth.AuthUserAttributeKey
 import com.example.milatronicsdashboard.databinding.ActivityUserBinding
+import com.google.android.material.snackbar.Snackbar
 
+// Host activity for the user fragments.
 class UserActivity : AppCompatActivity(){
     private lateinit var binding: ActivityUserBinding
     private lateinit var navController: NavController
@@ -15,18 +18,25 @@ class UserActivity : AppCompatActivity(){
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityUserBinding.inflate(layoutInflater)
-        setContentView(binding.root)
 
-        val navHostFragment = supportFragmentManager
-            .findFragmentById(R.id.nav_host_fragment_content_login) as NavHostFragment
-        navController = navHostFragment.navController
-
+        // Get the user details
         email = intent.getStringExtra(AuthUserAttributeKey.email().keyString).toString()
         name = intent.getStringExtra(AuthUserAttributeKey.name().keyString).toString()
 
-//        Snackbar.make(binding.root, "Logged in as $name", Snackbar.LENGTH_SHORT).show()
+        // Inflate and set the view.
+        binding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Display a snackbar displaying the username of the current user.
+        Snackbar.make(binding.snackbarLayout, "Logged in as $name", Snackbar.LENGTH_SHORT).show()
+
+        // Initialize the navigation controller
+        navController = (supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment).navController
+
+        // Set up the bottom navigation bar to use the NavController.
+        binding.bottomNavigationBar.setupWithNavController(navController)
     }
+
 
     override fun onSupportNavigateUp(): Boolean {
         return navController.navigateUp() || super.onSupportNavigateUp()
